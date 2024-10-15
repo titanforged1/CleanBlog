785cef010d108772608240312b5b3438dcd9e795
titanforged1 <sebnemozeski@gmail.com>
2024-10-15 13:55:52 +0300

    Workspace created

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-const fn = function () {
-  console.log('function working');
-};
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => {
+  const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
+  res.send('Blog title');
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı`);
+});
